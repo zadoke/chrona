6df5c89f64eedf899618a99d20306cf2fac8abb6
zadoke <rafaelajs@pm.me>
2023-07-14 02:26:40 +0100

    feat: add Access-Control-Allow-Origin header
    - Use tower-http's `CorsLayer` to set Access-Control-Allow-Origin header
    - Update dependencies to include tower-http
    - Note: Configurable variable by user for origin still needs to be implemented

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,8 @@
 use axum::Router;
+use axum::http::Method;
 use reqwest::Client;
 use std::sync::Arc;
+use tower_http::cors::{Any, CorsLayer};
 
 use crate::routes::train::train_route;
 use crate::routes::station::station_search_route;
@@ -8,9 +10,14 @@ use crate::routes::schedule::station_arrival_route;
 use crate::routes::schedule::station_departure_route;
 
 pub fn create_app(client: Arc<Client>) -> Router {
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(Any);
+
     Router::new()
         .nest("/", train_route(client.clone()))
         .nest("/", station_search_route(client.clone()))
         .nest("/", station_arrival_route(client.clone()))
         .nest("/", station_departure_route(client.clone()))
+        .layer(cors)
 }
